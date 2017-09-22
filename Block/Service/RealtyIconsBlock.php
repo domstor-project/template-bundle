94f4dbcd4d6d7168b4d0c411989a9ef471d92f22
Dmitry Anikeev <dm.anikeev@gmail.com>
2017-09-22 12:01:31 +0700

    updated title provider

--- Block/Service/RealtyIconsBlock.php
+++ Block/Service/RealtyIconsBlock.php
@@ -17,6 +17,7 @@ use Domstor_Builder;
 use Domstor_Helper;
 use Domstor\TemplateBundle\Model\TitleProvider;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpKernel\Config\FileLocator;
 
 /**
  * Description of RealtyIconsBlock
@@ -24,52 +25,62 @@ use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
  * @author Dmitry Anikeev <anikeev.dmitry@outlook.com>
  */
 class RealtyIconsBlock extends AbstractBlockService
-{    
+{
     protected $parameters = [];
-    
+
     /**
      *
      * @var Domstor_Builder 
      */
     protected $builder;
+    
+    /**
+     *
+     * @var FileLocator 
+     */
+    protected $locator;
+    
+    /**
+     *
+     * @var TitleProvider 
+     */
+    protected $provider;
 
-    public function __construct($name, EngineInterface $templating, Domstor_Builder $builder, array $parameters)
+    public function __construct($name, EngineInterface $templating, Domstor_Builder $builder, array $parameters, FileLocator $locator, TitleProvider $provider)
     {
         parent::__construct($name, $templating);
         $this->builder = $builder;
         $this->parameters = $parameters;
+        $this->locator = $locator;
+        $this->provider = $provider;
     }
 
     public function configureSettings(OptionsResolver $resolver)
     {
         $resolver
-            ->setDefaults([
-                'org_id'=>1,
-                'location_id'=>2004,
-                'template'=>'DomstorTemplateBundle:Block:realtyicons.html.twig',
-                'cache_time'=> 86400,
-                'cache_type'=>'file',
-                'cache_dir'=>''
-            ])
+                ->setDefaults([
+                    'org_id' => null,
+                    'location_id' => null,
+                    'template' => 'DomstorTemplateBundle:Block:realtyicons.html.twig',
+                    'cache_time' => null,
+                    'cache_type' => null,
+                    'cache_dir' => null
+                ])
         ;
-    }
-    
-    public function load(BlockInterface $block)
-    {
-        $block->setSettings($this->parameters);
     }
 
     public function execute(BlockContextInterface $blockContext, Response $response = null)
     {
-        $block = $blockContext->getBlock();
+        $contextSettings = $blockContext->getSettings();
+        $cacheDir = $this->locator->locate($contextSettings['cache_dir'] ? $contextSettings['cache_dir'] : $this->parameters['cache_dir']);
         $domstor = $this->builder->build([
-            'org_id' =>$block->getSetting('org_id'),
-            'location_id' => $block->getSetting('location_id'),
+            'org_id' => $contextSettings['org_id'] ? $contextSettings['org_id'] : $this->parameters['org_id'],
+            'location_id' => $contextSettings['location_id'] ? $contextSettings['location_id'] : $this->parameters['location_id'],
             'cache' => [
-                'type' => $block->getSetting('cache_type'),
-                'time' => $block->getSetting('cache_time'),
-                'uniq_key' => (string)$block->getSetting('org_id'),
-                'options' => ['directory' => $block->getSetting('cache_dir')],
+                'type' => $contextSettings['cache_type'] ? $contextSettings['cache_type'] : $this->parameters['cache_type'],
+                'time' => $contextSettings['cache_time'] ? $contextSettings['cache_time'] : $this->parameters['cache_time'],
+                'uniq_key' => $contextSettings['org_id'] ? (string) $contextSettings['org_id'] : (string) $this->parameters['org_id'],
+                'options' => ['directory' => $cacheDir],
             ],
         ]);
 
@@ -78,17 +89,20 @@ class RealtyIconsBlock extends AbstractBlockService
         $countsCom = [];
         $object = 'commerce';
         $countsCom[$object] = [];
-        foreach (Domstor_Helper::getActions($object) as $action) {
+        foreach (Domstor_Helper::getActions($object) as $action)
+        {
             $c = $domstor->getCount($object, $action);
-            if ($c > 0) {
+            if ($c > 0)
+            {
                 $countsCom[$object][$action] = $c;
             }
         }
-        
+
         return $this->renderResponse($blockContext->getTemplate(), [
-            'countsLiv' => $countsLiv,
-            'countsCom' => $countsCom,
-            'tp' => new TitleProvider($domstor),
+                    'countsLiv' => $countsLiv,
+                    'countsCom' => $countsCom,
+                    'tp' => $this->provider,
         ]);
     }
+
 }
--- Model/TitleProvider.php
+++ Model/TitleProvider.php
@@ -7,8 +7,9 @@
  */
 
 namespace Domstor\TemplateBundle\Model;
+
 use Domstor\TemplateBundle\Model\TitleProviderInterface;
-use Domstor_Domstor;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Description of TitleProvider
@@ -17,265 +18,58 @@ use Domstor_Domstor;
  */
 class TitleProvider implements TitleProviderInterface
 {
-    /**
-     *
-     * @var Domstor_Domstor
-     */
-    private $domstor;
-        
-    public function __construct(Domstor_Domstor $domstor) {
-        $this->domstor = $domstor;
+    private $objects;
+    
+    private $objectsHtml;
+    
+    private $actions;
+    
+    private $translator;
+
+    public function __construct(array $objects, array $objectsHtml, array $actions, TranslatorInterface $translator)
+    {
+        $this->objects = $objects;
+        $this->objectsHtml = $objectsHtml;
+        $this->actions = $actions;
+        $this->translator = $translator;
     }
 
-    private $objects = array(
-        'flat' => 'Квартиры',
-        'house' => 'Дома и коттеджи',
-        'land' => 'Земля и дачи',
-        'garage' => 'Гаражи и парковки',
-        'office' => 'Офисная',
-        'trade' => 'Торговая',
-        'product' => 'Производственная',
-        'storehouse' => 'Складская',
-        'landcom' => 'Земля',
-        'other' => 'Прочее',
-        'complex' => 'Имущественные',
-    );
-    
-    private $objectsHtml = array(
-        'flat' => 'Квартиры',
-        'house' => 'Дома<br>Коттеджи',
-        'land' => 'Дачи<br>Земля',
-        'garage' => 'Гаражи<br>Парковки',
-        'office' => 'Офисная',
-        'trade' => 'Торговая',
-        'product' => 'Производственная',
-        'storehouse' => 'Складская',
-        'landcom' => 'Земля',
-        'other' => 'Прочее',
-        'complex' => 'Имущественные',
-    );
-    
-    private $actions = array(
-        'sale' => 'Продают',
-        'rent' => 'Сдают',
-        'purchase' => 'Купят',
-        'rentuse' => 'Снимут',
-        'exchange' => 'Обмен',
-        'new' => 'Новостройки',
-    );
-    
     public function getType($object)
     {
-        if (!isset($this->objects[$object])) {
+        if (!isset($this->objects[$object]))
+        {
             throw new \Exception('Unsupported type ' . $object);
         }
-        
+
         return $this->objects[$object];
     }
-    
+
     public function getTypeHtml($object)
     {
-        if (!isset($this->objectsHtml[$object])) {
+        if (!isset($this->objectsHtml[$object]))
+        {
             throw new \Exception('Unsupported type ' . $object);
         }
-        
+
         return $this->objectsHtml[$object];
-    }    
-    
+    }
+
     public function getAction($action)
     {
-        if (!isset($this->actions[$action])) {
+        if (!isset($this->actions[$action]))
+        {
             throw new \Exception('Unsupported action ' . $action);
         }
-        
+
         return $this->actions[$action];
     }
-    
-    public function getListTitle($object, $action) {
-        $locName = $this->domstor->getLocationName('pr');
-        $title = 'Недвижимость в %s';
-        switch ($object) {
-            case 'flat':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа квартир в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда квартир в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка квартир в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду квартир в %s';
-                        break;
-                    case 'exchange':
-                        $title = 'Обмен квартир в %s';
-                        break;
-                    case 'new':
-                        $title = 'Новостройки в %s';
-                        break;
-                }
-                break;
-            case 'house':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа домов в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда домов в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка домов в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду домов в %s';
-                        break;
-                    case 'exchange':
-                        $title = 'Обмен домов в %s';
-                        break;
-                }
-                break;
-            case 'garage':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа гаражей в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда гаражей в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка гаражей в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду гаражей в %s';
-                        break;
-                }
-                break;
-            case 'land':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа земли и дач в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда земли и дач в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка земли и дач в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду земли и дач в %s';
-                        break;
-                }
-                break;
-            case 'trade':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа торговых помещений в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда торговых помещений в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка торговых помещений в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду торговых помещений в %s';
-                        break;
-                }
-                break;
-            case 'office':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа офисных помещений в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда офисных помещений в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка офисных помещений в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду офисных помещений в %s';
-                        break;
-                }
-                break;
-            case 'product':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа производственных помещений в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда производственных помещений в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка производственных помещений в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду производственных помещений в %s';
-                        break;
-                }
-                break;
-            case 'storehouse':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа складских помещений в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда складских помещений в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка складских помещений в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду складских помещений в %s';
-                        break;
-                }
-                break;
-            case 'landcom':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа коммерческой земли в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда коммерческой земли в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка коммерческой земли в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду коммерческой земли в %s';
-                        break;
-                }
-                break;
-            case 'complex':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа имущественных комплексов в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда имущественных комплексов в %s';
-                        break;
-                }
-                break;
-            case 'other':
-                switch ($action) {
-                    case 'sale':
-                        $title = 'Продажа нежилых помещений в %s';
-                        break;
-                    case 'rent':
-                        $title = 'Аренда нежилых помещений в %s';
-                        break;
-                    case 'purchase':
-                        $title = 'Покупка нежилых помещений в %s';
-                        break;
-                    case 'rentuse':
-                        $title = 'Заявки на аренду нежилых помещений в %s';
-                        break;
-                }
-                break;
-        }
-        
-        return sprintf($title, $locName);
+
+    public function getListTitle($object, $action, $locName)
+    {
+        $domain = 'TitleProvider';
+        //$title = 'Недвижимость в %s';
+        $id = $object . '.' . $action . '.title';
+        return sprintf($this->translator->trans($id, [], $domain), $locName);
     }
+
 }
