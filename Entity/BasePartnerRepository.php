aff62ee4af5d5a58185df0e009b6d0ac6d90d493
Dmitry Anikeev <dm.anikeev@gmail.com>
2017-09-22 16:37:13 +0700

    added provider interfaces

--- Entity/BasePartnerRepository.php
+++ Entity/BasePartnerRepository.php
@@ -10,14 +10,29 @@ namespace Domstor\TemplateBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
 use Domstor\TemplateBundle\Model\BlockContentProviderInterface;
+use Domstor\TemplateBundle\Model\PartnerProviderInterface;
+use Knp\Bundle\PaginatorBundle\Definition\PaginatorAwareInterface;
+use Knp\Component\Pager\Paginator;
+use PDO;
 
 /**
  * Description of BasePartnerRepository
  *
  * @author Dmitry Anikeev <anikeev.dmitry@outlook.com>
  */
-abstract class BasePartnerRepository extends EntityRepository implements BlockContentProviderInterface
+abstract class BasePartnerRepository extends EntityRepository implements BlockContentProviderInterface, PaginatorAwareInterface, PartnerProviderInterface
 {
+    /**
+     *
+     * @var Paginator 
+     */
+    protected $paginator;
+
+    public function setPaginator(Paginator $paginator)
+    {
+        $this->paginator = $paginator;
+    }
+    
     public function findForHomePage($options)
     {
         $qb = $this->createQueryBuilder('p');
@@ -28,5 +43,30 @@ abstract class BasePartnerRepository extends EntityRepository implements BlockCo
             ->setMaxResults($options['count'])
         ;
         return $qb->getQuery()->getResult();
+    }
+    
+    public function findForListPage($page, $limit)
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->addSelect('image')
+            ->leftJoin('p.image', 'image')
+            ->orderBy('p.sorting', 'asc')
+        ;
+        
+        $query = $qb->getQuery();
+        return $this->paginator->paginate($query, $page, $limit);
+    }
+    
+    public function findForShowPage($id)
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->addSelect('image')
+            ->leftJoin('p.image', 'image')
+            ->where($qb->expr()->eq('e.id', ':id'))
+            ->setParameter('id', $id, PDO::PARAM_INT)
+        ;        
+        return $qb->getQuery()->getOneOrNullResult();
     }
 }
---
+++ Model/EmployeeProviderInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Domstor\TemplateBundle\Model;
+
+use Domstor\TemplateBundle\Model\EmployeeInterface;
+use Knp\Component\Pager\Pagination\AbstractPagination;
+
+/**
+ *
+ * @author Dmitry Anikeev <anikeev.dmitry@outlook.com>
+ */
+interface EmployeeProviderInterface
+{
+    /**
+     * 
+     * @param int $page
+     * @param int $limit
+     * @return AbstractPagination
+     */
+    public function findForListPage($page, $limit);
+    
+    /**
+     * 
+     * @param int $id
+     * @return EmployeeInterface|null Description
+     */
+    public function findForShowPage($id);
+}
